02264a7b857d4f879a38e0e29bba4d9251fc5c81
Queiroz <kalifor_dbel@hotmail.com>
2022-04-24 13:29:05 -0300

    Implementando classes de testes

---
+++ StoreContext.Domain/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using Flunt.Notifications;
+using System;
+
+namespace StoreContext.Domain.Entities
+{
+    public class Entity : Notifiable
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
---
+++ StoreContext.Domain/Enums/EOrderStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace StoreContext.Domain.Enums
+{
+    public enum EOrderStatus
+    {
+        WaitingPayment = 1,
+        WaitingDelivery = 2,
+        Canceled = 3
+    }
+}
