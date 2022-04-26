c5bed9346f7eaedd72d97caf6f610ba63310c2c8
Queiroz <kalifor_dbel@hotmail.com>
2022-04-25 22:49:57 -0300

    Implementando testes dos commands

---
+++ StoreContext.Test/StoreContext.Tests/Commands/CreateOrderCommandsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreContext.Domain.Commands;
+using System;
+
+namespace StoreContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateOrderCommandsTests
+    {
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void PedidoNãoDeveSerGeradoParaComandoInvalido()
+        {
+            var command = new CreateOrderCommand();
+            command.Customer = "";
+            command.ZipCode = "13411080";
+            command.PromoCode = "12345678";
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Validate();
+
+            Assert.AreEqual(command.Valid, false);
+        }
+    }
+}
